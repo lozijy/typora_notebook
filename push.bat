d91f4b347344a3027038e0652b25d6c4fe31e24f
lozijy <2411107302@qq.com>
2022-11-25 01:04:28 +0800

    11.25

--- push.bat
+++ push.bat
@@ -13,8 +13,8 @@ set /p declation=输入提交的commit信息:
 git commit -m "%declation%"
 echo;
  
-echo 将变更情况提交到远程自己分支：git push origin hlt
-git push origin hlt
+echo 将变更情况提交到远程自己分支：git push 
+git push 
 echo;
  
 echo 切换主分支：git checkout master
